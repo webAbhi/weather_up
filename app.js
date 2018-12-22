d529546f0f07d2a5a4b1bee64f96d7eb4b300541
proabhi <abhijeetk9911@gmail.com>
2018-12-22 21:19:38 +0530

    new files

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
+
 const express = require('express')
 const app=express()
-const request = require('request');
+const request = require('request')
+app.set("view engine","ejs");
+// app.use(express.static(__dirname + '/view'))
+app.get('/',function(req,res){
+  request('http://api.openweathermap.org/data/2.5/weather?q=kanpur&units=metric&APPID=2cb188848466dee1ad22eefdd09126aa',function(error,response,body){
+    if (!error && response.statusCode==200){
+      var newbody=JSON.parse(body)
 
 
-request('http://api.openweathermap.org/data/2.5/weather?q=bangalore&units=metric&APPID=2cb188848466dee1ad22eefdd09126aa',function(error,response,body){
-  if(error){
-    console.log('something is wrong')
-    console.log(error)
-  }
-  else if (response.statusCode==200){
-    let newbody=JSON.parse(body)
-    console.log(newbody["main"]['temp'])
-  }
+      // newbody["main"]['temp'];
+      var some="yello"
+      res.render("result",{some:some});
+      console.log('success')
+    }
+  })
 })
+
+
+
+
 
 app.listen(3000, () => console.log("app listening on port 3000"))
