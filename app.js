b514abf641357d380067d9ece2d4732d058cceb7
proabhi <abhijeetk9911@gmail.com>
2018-12-23 20:26:41 +0530

    working by manual cocode change

--- app.js
+++ app.js
@@ -2,24 +2,14 @@
 const express = require('express')
 const app=express()
 const request = require('request')
-app.set("view engine","ejs");
-// app.use(express.static(__dirname + '/view'))
-app.get('/',function(req,res){
-  request('http://api.openweathermap.org/data/2.5/weather?q=kanpur&units=metric&APPID=2cb188848466dee1ad22eefdd09126aa',function(error,response,body){
-    if (!error && response.statusCode==200){
-      var newbody=JSON.parse(body)
-
-
-      // newbody["main"]['temp'];
-      var some="yello"
-      res.render("result",{some:some});
-      console.log('success')
+app.set("view engine","ejs")
+app.get('/test',function(req,res){
+  request('http://api.openweathermap.org/data/2.5/weather?q=bangalore&units=metric&APPID=2cb188848466dee1ad22eefdd09126aa',function(error,response,body){
+    if(!error  &&  response.statusCode==200){
+      let newbody=JSON.parse(body)
+      let data=newbody["main"]['temp']
+      res.render('result',{data:data})
     }
   })
 })
-
-
-
-
-
 app.listen(3000, () => console.log("app listening on port 3000"))
