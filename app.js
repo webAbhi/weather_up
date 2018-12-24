a4a5fc018fc6c43ed779c97fd73c40ff9913780f
proabhi <abhijeetk9911@gmail.com>
2018-12-24 21:54:42 +0530

    interface adde

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express')
 const app=express()
 const request = require('request')
 const bodyParser = require('body-parser')
-let location="bangalore"
-app.use(express.json())
-app.use(express.urlencoded())
+app.use(bodyParser.urlencoded({extended:true}))
+let location="chennai"
+app.use(express.static(__dirname + '/views'))
+// app.use(express.json())
+// app.use(express.urlencoded())
 app.set("view engine","ejs")
 
 app.post('/run',function(req,res){
@@ -18,7 +20,10 @@ app.get('/', function (req,res){
   request('http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=metric&APPID=2cb188848466dee1ad22eefdd09126aa',function(error,response,body){
     if(!error  &&  response.statusCode==200){
       let newbody=JSON.parse(body)
-      let data=newbody["main"]['temp']
+      let data={
+        "temp":newbody["main"]['temp'],
+        "name":newbody["name"]
+      };
       res.render('result',{data:data})
       console.log(data)
     }
