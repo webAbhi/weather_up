843b3b3118ecc00e722977dd82b49864b26a2b80
proabhi <abhijeetk9911@gmail.com>
2018-12-28 22:03:18 +0530

    key fixed by env

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app=express()
 const request = require('request')
+const dotenv=require('dotenv').config()
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:true}))
 let location="chennai"
@@ -13,11 +14,9 @@ app.set("view engine","ejs")
 app.post('/run',function(req,res){
   location=req.body.place
   res.redirect('/')
-})
-
+});
 app.get('/', function (req,res){
-
-  request('http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=metric&APPID=2cb188848466dee1ad22eefdd09126aa',function(error,response,body){
+  request('http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=metric&APPID='+process.env.WEATHER_KEY,function(error,response,body){
     if(!error  &&  response.statusCode==200){
       let newbody=JSON.parse(body)
       let data={
@@ -29,10 +28,5 @@ app.get('/', function (req,res){
     }
   })
 })
-// navigator.geolocation.getCurrentPosition(function(location) {
-//   console.log(location.coords.latitude);
-//   console.log(location.coords.longitude);
-//   console.log(location.coords.accuracy);
-// });
 
 app.listen(3000, () => console.log("app listening on port 3000"))
--- views/js/page.js
+++ views/js/page.js
@@ -1,22 +1,39 @@
 function geoFindMe(){
   function positionFind(position){
-    console.log(position.coords.latitude)
-    console.log(position.coords.longitude)
+    //lat and long value passed in the geo function
     geo(position.coords.latitude,position.coords.longitude)
   }
+  //check if browswer support geolocation
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(positionFind)
   }
 }
 function geo (lat,long){
+  //axios is used to make a request from js file
   axios.get('https://maps.googleapis.com/maps/api/geocode/json?',{
     params:{
       latlng:lat+','+long,
-      key:'',
+      key:MAP_KEY,
+      result_type:'political',
     }
   }).then((response)=>{
-    console.log(response.data.results[3].formatted_address)
+    console.log(response.data.results[1].address_components[0].long_name)
+    document.querySelector('.message').innerHTML=response.data.results[1].address_components[0].long_name;
+    currentTemp(response.data.results[1].address_components[0].long_name)
   }).catch((error)=> {
     console.log(error);
+  })
+}
+function currentTemp(place){
+  axios.get('http://api.openweathermap.org/data/2.5/weather?',{
+    params:{
+      q:place,
+      units:"metric",
+      APPID:WEATHER_KEY
+    }
+  }).then((response)=>{
+    document.getElementById('temp').textContent=response.data.main.temp;
+    document.getElementById('location').textContent=place;
+    document.querySelector('.message').classList.add('apply');
   })
 }
