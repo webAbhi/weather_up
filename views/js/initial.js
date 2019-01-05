292a9ee80d4a9b1e0b560c42160ca8dd8c748b2e
proabhi <abhijeetk9911@gmail.com>
2019-01-05 22:00:44 +0530

    footer added

--- views/js/initial.js
+++ views/js/initial.js
@@ -9,15 +9,12 @@ window.onload = function () {
     navigator.geolocation.getCurrentPosition(positionFind)
   }
 }
-let info={}
 async function weather(lat,lon){
   // API call
   let data=await fetch('http://api.openweathermap.org/data/2.5/weather?'+'&lat='+lat+'&lon='+lon+'&units=metric'+'&APPID='+WEATHER_KEY)
-  console.log("inside API")
   const res= await data.json();
-  console.log(info.temp)
   document.querySelector('li:nth-child(1)').innerHTML="Location:"+res.name+', '+res.sys.country;
-  document.querySelector('li:nth-child(2)').innerHTML="Temperature:"+res.main.temp+"<sup>0</sup>C";
+  document.querySelector('li:nth-child(2)').innerHTML="Temperature:"+res.main.temp+"<sup>o</sup>C";
   document.querySelector('li:nth-child(3)').innerHTML="Description:"+res.weather[0].description;
   document.querySelector('li:nth-child(4)').innerHTML="Humidity:"+res.main.humidity;
   document.querySelector('li:nth-child(5)').innerHTML="Pressure:"+res.main.pressure+" hpa";
