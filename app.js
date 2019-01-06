058119be184ef2c1a3e98f921ad6daf162c0c97b
proabhi <abhijeetk9911@gmail.com>
2019-01-06 13:27:34 +0530

    port changed

--- app.js
+++ app.js
@@ -35,4 +35,4 @@ app.get('/foam',function (req,res){
     }
   })
 })
-app.listen(3000, () => console.log("app listening on port 3000"))
+app.listen(process.env.PORT||3000, () => console.log("app listening on port 3000"))
