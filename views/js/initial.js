5d7766ced371d63b68470dc4dff8e47297c42241
proabhi <abhijeetk9911@gmail.com>
2019-01-06 12:45:16 +0530

    minor change

--- views/js/initial.js
+++ views/js/initial.js
@@ -13,9 +13,9 @@ async function weather(lat,lon){
   // API call
   let data=await fetch('http://api.openweathermap.org/data/2.5/weather?'+'&lat='+lat+'&lon='+lon+'&units=metric'+'&APPID='+WEATHER_KEY)
   const res= await data.json();
-  document.querySelector('li:nth-child(1)').innerHTML="Location:"+res.name+', '+res.sys.country;
-  document.querySelector('li:nth-child(2)').innerHTML="Temperature:"+res.main.temp+"<sup>o</sup>C";
-  document.querySelector('li:nth-child(3)').innerHTML="Description:"+res.weather[0].description;
-  document.querySelector('li:nth-child(5)').innerHTML="Humidity:"+res.main.humidity;
-  document.querySelector('li:nth-child(4)').innerHTML="Pressure:"+res.main.pressure+" hpa";
+  document.querySelector('li:nth-child(1)').innerHTML="Location: "+res.name+', '+res.sys.country;
+  document.querySelector('li:nth-child(2)').innerHTML="Temperature: "+res.main.temp+"<sup>o</sup>C";
+  document.querySelector('li:nth-child(3)').innerHTML="Description: "+res.weather[0].description;
+  document.querySelector('li:nth-child(5)').innerHTML="Humidity: "+res.main.humidity;
+  document.querySelector('li:nth-child(4)').innerHTML="Pressure: "+res.main.pressure+" hpa";
 }
